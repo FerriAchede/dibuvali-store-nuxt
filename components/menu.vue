dc6e40fc7b75b59c0a7afa302df3e4589c69dc46
FerriAchede <ferriyt38@gmail.com>
2025-04-17 21:34:51 +0200

    Refactor estilos y estructura del componente Menu: mejorar la presentación y funcionalidad del menú móvil, incluyendo transiciones y ajustes de diseño.

--- components/menu.vue
+++ components/menu.vue
@@ -1,7 +1,7 @@
 <template>
-      <header class="header-desktop d-none d-md-flex">
+    <header class="hidden md:flex header-desktop">
         <div class="left-icons">
-            <Icon name="mingcute:search-2-fill" href="https://icones.js.org/collection/mingcute?s=sear&icon=mingcute:search-2-fill"/>
+            <Icon name="mingcute:search-2-fill" />
         </div>
 
         <div class="logo-nav-container">
@@ -17,22 +17,57 @@
         </div>
 
         <div class="icons">
-            <Icon name="mingcute:user-4-fill"/>
-            <Icon name="mingcute:shopping-cart-2-fill"/>
+            <Icon name="mingcute:user-4-fill" />
+            <Icon name="mingcute:shopping-cart-2-fill" />
         </div>
     </header>
+
+    <header class="md:hidden h-auto p-6">
+        <div class="flex justify-between items-center">
+            <button @click="showMenu = !showMenu" aria-label="Toggle navigation">
+                <Icon name="mingcute:align-justify-fill" class="open-nav-icon" />
+            </button>
+
+            <div class="mx-6">
+                <NuxtLink to="/">
+                    <img src="/logotxt.webp" alt="Dibu Vali" class="profile-pic-mobile" />
+                </NuxtLink>
+            </div>
+
+            <div class="">
+                <NuxtLink to="/carrito">
+                    <Icon name="mingcute:shopping-cart-2-fill" />
+                </NuxtLink>
+                <NuxtLink to="/busqueda">
+                    <Icon name="mingcute:search-2-fill" />
+                </NuxtLink>
+            </div>
+        </div>
+
+
+    </header>
+    <Transition name="slide-down">
+        <nav v-if="showMenu" class="navigation flex flex-col gap-3 py-4 pt-0 pb-3 px-7">
+            <NuxtLink to="/" class="nav-link">Inicio</NuxtLink>
+            <NuxtLink to="/new" class="nav-link">Nuevo</NuxtLink>
+            <NuxtLink to="/products" class="nav-link">Productos</NuxtLink>
+            <NuxtLink to="/collections" class="nav-link">Colecciones</NuxtLink>
+            <NuxtLink to="/#about-me" class="nav-link">Sobre mí</NuxtLink>
+            <NuxtLink to="/contacto" class="nav-link">Contacto</NuxtLink>
+        </nav>
+    </Transition>
+
 </template>
-<script>
-export default {
-    
-}
+<script setup>
+
+const showMenu = ref(false)
+
 </script>
 <style scoped>
-header {
+.header-desktop {
     background-color: var(--color-background);
     width: 100%;
     height: 145px;
-    display: flex;
     align-items: center;
     justify-content: space-between;
     padding: 10px 50px;
@@ -107,5 +142,52 @@ header.shrink-more {
     color: var(--color-morado-hover);
 }
 
+.open-nav-icon {
+    height: 32px;
+    width: 32px;
+}
 
+#mobileNavbar {
+    position: sticky;
+    top: 9%;
+    width: 100%;
+    background-color: var(--color-background-content);
+    box-shadow: 0 3px 5px rgba(0, 0, 0, 0.3);
+    z-index: 3;
+}
+
+#mobileNavbar.shrink-more {
+    top: 5%;
+}
+
+
+.slide-down-enter-active,
+.slide-down-leave-active {
+    transition: all 0.3s ease;
+    overflow: hidden;
+}
+
+.slide-down-enter-from {
+    max-height: 0;
+    opacity: 0;
+    transform: translateY(-10px);
+}
+
+.slide-down-enter-to {
+    max-height: 500px;
+    opacity: 1;
+    transform: translateY(0);
+}
+
+.slide-down-leave-from {
+    max-height: 500px;
+    opacity: 1;
+    transform: translateY(0);
+}
+
+.slide-down-leave-to {
+    max-height: 0;
+    opacity: 0;
+    transform: translateY(-10px);
+}
 </style>
