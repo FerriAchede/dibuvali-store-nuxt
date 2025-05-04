262356440266634be65d40bdf6df3c957799df9a
FerriAchede <ferriyt38@gmail.com>
2025-05-04 17:17:26 +0200

    Agregar componente ProductCard: implementar la visualización de productos con imágenes de hover y estructura de estilos. Mejorar la página de productos con carga dinámica y manejo de estado.

---
+++ components/ProductCard.vue
@@ -0,0 +1,126 @@
+<script setup>
+const props = defineProps({
+    product: {
+        type: Object,
+        required: true,
+    },
+});
+
+const isHovered = ref(false);
+
+const setHoverImage = () => {
+    isHovered.value = true;
+};
+
+const resetImage = () => {
+    isHovered.value = false;
+};
+</script>
+
+<template>
+    <div class="w-full">
+        <div
+            class="product-item"
+            @mouseenter="product.hover_image && setHoverImage()"
+            @mouseleave="product.hover_image && resetImage()">
+            <div class="image-wrapper">
+                <img
+                    :src="product.image"
+                    :alt="product.title"
+                    class="base-img"
+                    :class="{ 'fade-out': isHovered && product.hover_image }" />
+                <img
+                    v-if="product.hover_image"
+                    :src="product.hover_image"
+                    :alt="product.title + ' hover'"
+                    class="hover-img"
+                    :class="{ 'fade-in': isHovered }" />
+            </div>
+            <p class="mt-1">{{ product.title }}</p>
+            <span>{{ product.price }} {{ product.currency_code }}</span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.product-item {
+    position: relative;
+    text-align: center;
+    background-color: var(--color-background);
+    width: 220px;
+    height: auto;
+    margin: 0 auto;
+    border-radius: 16px;
+    padding: 10px;
+    margin-bottom: auto;
+}
+  
+.product-item p {
+    margin-bottom: 0;
+}
+
+.product-item img {
+    width: 200px;
+    height: 200px;
+    object-fit: cover;
+    border-radius: 8px;
+    box-shadow: 0px 2px 5px var(--color-morado-hover);
+    transition: transform 0.3s ease, box-shadow 0.3s ease, translate 0.3s ease;
+}
+
+.product-item img:hover {
+    transform: scale(1) translateY(-1px);
+    box-shadow: 0px 1px 6px var(--color-azul-celeste);
+}
+
+.product-item p,
+span {
+    color: var(--color-morado);
+    padding-top: 8px;
+}
+
+.product-item p {
+    font-weight: 500;
+}
+.product-item span {
+    color: #813bfbd8;
+    font-weight: 600;
+}
+
+.image-wrapper {
+    position: relative;
+    width: 200px;
+    height: 200px;
+    margin: 0 auto;
+}
+
+.image-wrapper img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 200px;
+    height: 200px;
+    object-fit: cover;
+    border-radius: 8px;
+    transition: opacity 0.5s ease;
+    box-shadow: 0px 2px 5px var(--color-morado-hover);
+}
+
+.base-img {
+    z-index: 1;
+    opacity: 1;
+}
+
+.hover-img {
+    z-index: 2;
+    opacity: 0;
+}
+
+.fade-in {
+    opacity: 1 !important;
+}
+
+.fade-out {
+    opacity: 0 !important;
+}
+</style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,5 +1,56 @@
+<script setup>
+const { data: products, status } = await useFetch(
+    "http://localhost:8000/api/products",
+    {
+        lazy: true,
+    }
+);
+</script>
+
 <template>
-    <div>
-        <h1>Products</h1>
+    <section v-if="products?.data" class="products text-center py-10">
+        <div class="container mx-auto px-4">
+            <h2>Products</h2>
+
+            <div
+                class="product-grid grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-0">
+                <div v-if="status === 'pending'" class="w-full text-center">
+                    <p>Loading... Icono de carga</p>
+                </div>
+                <ProductCard
+                    v-else
+                    v-for="product in products.data"
+                    :key="product.id"
+                    :product="product" />
+            </div>
+        </div>
+    </section>
+
+    <div v-else>
+        <p>No products available.</p>
     </div>
 </template>
+
+<style scoped>
+.product-grid {
+    max-width: 1400px;
+    margin: 0 auto;
+    padding: 30px 30px 0px;
+}
+
+.products {
+    padding: 30px 30px;
+    text-align: center;
+    background-color: var(--color-background-content);
+    max-width: 1400px;
+    margin: 0 auto;
+    box-shadow: -20px 0 15px -5px #ffeae9b3,
+                20px 0 15px -5px #ffeae9b3;
+}
+
+.products h2 {
+    color: var(--color-morado);
+    font-size: 24px;
+    font-weight: bold;
+}
+</style>
